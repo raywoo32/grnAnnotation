575b8ec8cb5405fc9923cce1c90b7e9b220a6e1e
raywoo32 <40863729+raywoo32@users.noreply.github.com>
2019-10-21 14:44:37 -0400

    tested,
    added default image (test.png) when na is used as imagePath. 
    added auto categorizing of tags

---
+++ uploadOct212019.sh
@@ -0,0 +1,11 @@
+# BASH ALL UPLOAD COMMANDS, didn't automate in case of reuploads 
+
+#locally tested
+node ../insertSIF.js 17237218.sif 17237218.jpg 
+node ../insertSIF.js 25736223#1.sif 25736223#1.jpg
+node ../insertSIF.js 25736223#2.sif 25736223#2.jpg
+node ../insertSIF.js 26247122.sif na
+node ../insertSIF.js 27923776.sif 27923776#2.jpg
+node ../insertSIF.js 29462363.sif 29462363.jpg
+node ../insertSIF.js 30616516.sif 30616516.jpg
+node ../insertSIF.js 31595065.sif na
